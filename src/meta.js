8073b3bdd1e1b339a77a9c545cb0628234fcd81e
oliezekat <oliezekat@yahoo.fr>
2024-07-02 21:52:09 +0200

    Initial source from greasyfork.org/456297 (version 1.1940)
    split userscript's meta and main Javascript source-code

---
+++ src/meta.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name                 Empire Overview
+// @name:en              Empire Overview
+// @name:el              Empire Overview
+// @author               germano / 1.17 to 1.18 by Ariston /up 1.18 germano by mrFiX+(Thanx my friend) /up 1.9xx
+// @description          Script for Ikariam 8.x.x, Overview tables for resources, buildings and military inspired by Ikariam Empire Board
+// @description:en       Script for Ikariam 8.x.x, Overview tables for resources, buildings and military inspired by Ikariam Empire Board
+// @description:el       Script for Ikariam 8.x.x, Overview tables for resources, buildings and military inspired by Ikariam Empire Board
+// @namespace            Beta
+// @grant                unsafeWindow
+// @grant                GM_getValue
+// @grant                GM_setValue
+// @grant                GM_deleteValue
+// @grant                GM_addStyle
+// @grant                GM_registerMenuCommand
+// @grant                GM_xmlhttpRequest
+// @grant                GM_openInTab
+//
+// @exclude              http://board.*.ikariam.gameforge.com*
+// @exclude              http://*.ikariam.gameforge.*/board
+// @include	             /https?:\/\/s[0-9]*-[a-z]{2}\.ikariam\.gameforge\.com\/.*/
+//
+// @require              https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+// @require              https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js
+//
+// @version              1.1940
+//
+// @license              GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @icon https://www.google.com/s2/favicons?domain=ikariam.com
+// @downloadURL https://update.greasyfork.org/scripts/456297/Empire%20Overview.user.js
+// @updateURL https://update.greasyfork.org/scripts/456297/Empire%20Overview.meta.js
+// ==/UserScript==
